be4dbea39782c5b1baa17579a35e253d7aa14b4e
ayacomputer <ayako.woollan@gmail.com>
2026-02-22 23:54:35 +1100

    Add configuration file for application settings and constants

---
+++ config.js
@@ -0,0 +1,14 @@
+export const YTDLP = process.env.YTDLP_PATH || '/usr/local/bin/yt-dlp';
+
+export const COLORS = {
+  nowPlaying: 0x9B59B6,
+  added:      0x00FF99,
+  queue:      0x7D3C98,
+  error:      0xFF4D6D,
+  neutral:    0x2C2F33,
+};
+
+export const PROGRESS_INTERVAL_MS = 5_000;
+export const MAX_VOLUME = 2;
+export const MIN_VOLUME = 0;
+export const VOICE_READY_TIMEOUT_MS = 30_000;
